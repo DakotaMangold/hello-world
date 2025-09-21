a11e60c73ec58215a8c51e7c9066d4ae8e88c430
DakotaMangold <150461901+DakotaMangold@users.noreply.github.com>
2025-09-20 19:49:27 -0500

    Add files via upload

---
+++ SDEV220M04.py
@@ -0,0 +1,10 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import zoo
+>>> zoo.hours
+<function hours at 0x000001E2DA7B0220>
+>>> zoo.hours()
+Open 9-5 daily
+>>> import zoo as menagerie
+>>> menagerie.hours()
+Open 9-5 daily
